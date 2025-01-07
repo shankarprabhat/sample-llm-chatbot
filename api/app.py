8cd82dfe2a922cc29030d494bb795fcb4f382ba7
prabhat shankar <prabhat4686@gmail.com>
2025-01-07 18:59:22 +0530

    used huggingface inference

--- api/app.py
+++ api/app.py
@@ -12,21 +12,42 @@ CORS(app)  # Enable CORS for all routes
 
 # Load the LLM model (adjust as needed)
 # chatbot = pipeline("conversational", model="gpt-2")
-big_model = "deepset/roberta-base-squad2"
-small_model = "deepset/tinyroberta-squad2"
-chatbot = pipeline("question-answering", model=small_model)
+# big_model = "deepset/roberta-base-squad2"
+# small_model = "deepset/tinyroberta-squad2"
+# chatbot = pipeline("question-answering", model=small_model)
 
 @app.route("/chat", methods=["POST"])
 def chat():
+    # print('Request:', request.json)
+    # user_input = request.json.get("message")
+    
+    # QA_input = {
+    # "question": user_input,
+    # "context": "LLM provides the capability to understand languages."
+    # }
+    # response = chatbot(QA_input)
+    # return jsonify({"response": response['answer']})
+
+    import requests
+    # from pprint import pprint
+
+    TOKEN = "Bearer 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"
+
+    API_URL = 'https://api-inference.huggingface.co/models/deepset/roberta-base-squad2'
+    headers = {"Authorization": TOKEN}
+
     print('Request:', request.json)
     user_input = request.json.get("message")
-    
-    QA_input = {
-    "question": user_input,
-    "context": "LLM provides the capability to understand languages."
-    }
-    response = chatbot(QA_input)
-    return jsonify({"response": response['answer']})
+
+    # The Entertheaccesskeyhere is just a placeholder, which can be changed according to the user's access key
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    params = {'max_length': 200, 'top_k': 10, 'temperature': 2.5}
+    output = query({'inputs': {"question": user_input,"context": "My name is Suvojit and I am a Senior Data Scientist"},'parameters': params})
+    # pprint('output',output)
+    return jsonify({"response": output[0]['answer']})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- run_local.py
+++ run_local.py
@@ -1,13 +1,37 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
+call_pipeline = False
 # model_name = "deepset/roberta-base-squad2"
-model_name = "deepset/tinyroberta-squad2"
 
-# a) Get predictions
-nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
-QA_input = {
-    'question': 'What is LLM?',
-    'context': 'LLM provides the capability to understand languages.'
-}
-res = nlp(QA_input)
-print(res['answer'])
+if call_pipeline is True:
+    model_name = "deepset/tinyroberta-squad2"
+
+    # a) Get predictions
+    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    QA_input = {
+        'question': 'What is LLM?',
+        'context': 'LLM provides the capability to understand languages.'
+    }
+    res = nlp(QA_input)
+    print(res['answer'])
+
+call_inference_api = True
+
+if call_inference_api is True:
+    import requests
+    from pprint import pprint
+
+    TOKEN = "Bearer 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"
+
+    API_URL = 'https://api-inference.huggingface.co/models/deepset/roberta-base-squad2'
+    headers = {"Authorization": TOKEN}
+
+    # The Entertheaccesskeyhere is just a placeholder, which can be changed according to the user's access key
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    params = {'max_length': 200, 'top_k': 10, 'temperature': 2.5}
+    output = query({'inputs': {"question": "What's my profession?","context": "My name is Suvojit and I am a Senior Data Scientist"},'parameters': params})
+    # pprint(output)
+    print(output[0]['answer'])
